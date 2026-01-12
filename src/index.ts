3fbd823b1170811c6f1f41a69b3f9d643896d6c8
vssouza <vs.souza@gmail.com>
2026-01-12 15:22:06 +0000

    deployment retrigger

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { buildApp } from "./app.js";
+import {buildApp} from "./app.js";
 
 const port = Number(process.env.PORT) || 3000;
 const host = "0.0.0.0";
@@ -7,7 +7,7 @@ const app = buildApp();
 
 await app.ready();
 
-app.listen({ port, host }).catch((err) => {
+app.listen({port, host}).catch((err) => {
   app.log.error(err);
   process.exit(1);
 });
